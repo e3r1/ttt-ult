bb455c7a9c8527023dacc0df43b793cf901e7006
e3r1 <elivarhost@gmail.com>
2026-01-10 21:26:48 +0700

    fix KEN

--- ultimate_tic_tac_toe.gd
+++ ultimate_tic_tac_toe.gd
@@ -18,40 +18,34 @@ var color2 = Color(0.187, 0.2, 0.213, 1.0)
 var color2_focused = Color(0.075, 0.082, 0.089, 1.0)
 
 func _ready() -> void:
-	boards_winner.resize(9)
-	
-	#создаем поле досок
-	for j in range(3):
-		boards_color.append([])
-		for i in range(3):
-			boards_color[j].append(color2)
-			$option_board_number.add_item(str(3*j+i+1))
-	#конец создания поля досок
-	
-	#создаем поле кнопок
-	$game_grid.columns = 9
+	$game_grid.columns = 3
 	var style = StyleBoxFlat.new()
 	style.bg_color = color2
+	field_x.create(Vector2i(9,9))
+	field_o.create(Vector2i(9,9))
+	boards_winner.resize(9)
 	
-	for j in range(9):
-		for i in range(9):
-			var button = Button.new()
-			button.text = " "
-			button.position = Vector2(i,j)
-			button.custom_minimum_size = Vector2(50,50)
-			button.pressed.connect(click_button.bind(button, i, j))
-			button.add_theme_stylebox_override("normal", style)
-			button.add_theme_color_override("font_color", Color.WHITE)
-			$game_grid.add_child(button)
-	#конец создания поля кнопок
+	for n in range(9):
+		var grid = GridContainer.new()
+		grid.columns = 3
+		for j in range(3):
+			for i in range(3):
+				var button = Button.new()
+				var local_n = i + j*3
+				button.text = " "
+				button.position = Vector2(i,j)
+				button.custom_minimum_size = Vector2(50,50)
+				button.pressed.connect(click_button.bind(n, local_n))
+				button.add_theme_stylebox_override("normal", style)
+				button.add_theme_color_override("font_color", Color.WHITE)
+				grid.add_child(button)
+		$game_grid.add_child(grid)
+		
+		boards_color.append(color2)
+		$option_board_number.add_item(str(n))
 	
 	draw_lines()
 	draw_coordinates()
-	
-	#создаем поле значений
-	field_x.create(Vector2i(9,9))
-	field_o.create(Vector2i(9,9))
-	#конец создания поля значений
 
 #func _process(delta: float) -> void:
 #	pass
@@ -59,13 +53,10 @@ func _ready() -> void:
 func check_win_condition(board_number: int) -> String:
 	var fx = []
 	var fo = []
-	var board_x = board_number%3
-	var board_y = board_number/3
 	
-	for y in 3:
-			for x in 3:
-				fx.append(field_x.get_bit(board_x*3 + x,board_y*3 + y))
-				fo.append(field_o.get_bit(board_x*3 + x,board_y*3 + y))
+	for i in range(9):
+		fx.append(field_x.get_bit(i, board_number))
+		fo.append(field_o.get_bit(i, board_number))
 	
 	var s = fx
 	for i in range(9):
@@ -80,21 +71,16 @@ func check_win_condition(board_number: int) -> String:
 		(s[0]&&s[4]&&s[8]) || (s[2]&&s[4]&&s[6])):
 			return "o"
 	
-	var is_board_full = check_full_board(board_x, board_y)
-	if is_board_full:
+	if check_full_board(board_number):
 		return "draw"
 	
 	return "playing"
 
-func check_full_board(board_x: int, board_y) -> bool:
-	var fx = []
-	var fo = []
-	for y in 3:
-			for x in 3:
-				fx.append(field_x.get_bit(board_x + x,board_y + y))
-				fo.append(field_o.get_bit(board_x + x,board_y + y))
+func check_full_board(board_number: int) -> bool:
 	for i in range(9):
-		if !(fx[i]||fo[i]): 
+		var bit_x = field_x.get_bit(i, board_number*9)
+		var bit_o = field_o.get_bit(i, board_number*9)
+		if !(bit_x || bit_o):
 			return false
 	return true
 
@@ -120,52 +106,38 @@ func clear_all() -> void:
 	var style = StyleBoxFlat.new()
 	style.bg_color = bg_color
 	
-	for button in $game_grid.get_children():
-		button.text = ""
-		button.add_theme_stylebox_override("normal", style)
-	for j in range(3):
-		for i in range(3):
-			boards_color[i][j] = bg_color
+	for grid in $game_grid.get_children():
+		for button in grid.get_children():
+			button.text = ""
+			button.add_theme_stylebox_override("normal", style)
+			
+	for n in range(9):
+		boards_color[n] = bg_color
 	print_win_count()
 	print_KEN()
 
-func set_next_board() -> void:
-	if (last_move != ""):
-		var next_board_x = "abcdefghi".find(last_move[0])
-		var next_board_y = 9 - int(last_move[1])
-		var next_board = next_board_x%3 + next_board_y%3*3
-		
-		if check_full_board(next_board_x, next_board_y) || boards_winner[next_board]:
-			$label_next_board/label_value.text = "any"
-			remove_focus_on_board(focused_board)
-		else:
-			$label_next_board/label_value.text = str(next_board+1)
-			focus_on_board(next_board)
-	
-	pass
-
-func click_button(button, x, y) -> void:
-	var board_number = y/3*3 + x/3
+func click_button(board_number, local_n) -> void:
 	$option_board_number.select(board_number)
 	
-	var is_button_empty = !(field_x.get_bit(x,y) || field_o.get_bit(x,y))
+	var global_position = board_number * 9 + local_n
+	var global_x = global_position%9
+	var global_y = global_position/9
+	var is_button_empty = !(field_x.get_bit(global_x, global_y) || field_o.get_bit(global_x, global_y))
 	var is_board_targeted = $label_next_board/label_value.text == "any" || $label_next_board/label_value.text == str(board_number + 1)
 	var is_board_game_ended = boards_winner[board_number]
-	var is_game_over = (win_count_x >= 3) || (win_count_o >= 3)
+	var is_game_over = (win_count_x >= 4) || (win_count_o >= 4)
 	if is_button_empty && is_board_targeted && !is_board_game_ended && !is_game_over:
-		move(x, y, button)
+		move(board_number, local_n)
 		handle_board_result(board_number)
 		set_next_board()
 		print_KEN()
 
 func draw_bg_color(board_number: int, bg_color: Color) -> void:
-	var buttons = $game_grid.get_children()
+	var buttons = $game_grid.get_child(board_number).get_children()
 	var style = StyleBoxFlat.new()
 	style.bg_color = bg_color
-	for btn_j in 3:
-			for btn_i in 3:
-				buttons[btn_j*9 + board_number*3 + (board_number/3)*18 + btn_i
-				].add_theme_stylebox_override("normal", style)
+	for button in buttons:
+		button.add_theme_stylebox_override("normal", style)
 
 func draw_lines() -> void:
 	var line = Line2D.new()
@@ -205,16 +177,16 @@ func draw_coordinates() -> void:
 		label.text = string[i]
 		label.position = Vector2(x + 20 + i*54, y + 484)
 
-func focus_on_board(i: int) -> void:
+func focus_on_board(board_number: int) -> void:
 	remove_focus_on_board(focused_board)
-	var board_color = boards_color[i/3][i%3]
+	var board_color = boards_color[board_number]
 	if board_color == color0:
-		draw_bg_color(i, color0_focused)
+		draw_bg_color(board_number, color0_focused)
 	else: if board_color == color1:
-		draw_bg_color(i, color1_focused)
+		draw_bg_color(board_number, color1_focused)
 	else: if board_color == color2:
-		draw_bg_color(i, color2_focused)
-	focused_board = i
+		draw_bg_color(board_number, color2_focused)
+	focused_board = board_number
 
 func handle_board_result(board_number: int) -> void:
 	var board_result = check_win_condition(board_number)
@@ -231,18 +203,18 @@ func handle_board_result(board_number: int) -> void:
 func import_KEN() -> void:
 	clear_all()
 	var current_KEN = $textEdit_import_KEN.text
-	var i = 0
-	var j = 8
+	var board_n = 0
+	var local_n = 0
 	for s in current_KEN:
 		if s == "x" || s == "o":
 			who_turn = s
-			move(i, j)
-			i += 1
+			move(transform_to_reverse_y_order(board_n), transform_to_reverse_y_order(local_n))
+			local_n += 1
 		else: if("123456789".find(s) != -1):
-			i += int(s)
+			local_n += int(s)
 		else: if s == "-":
-			i = 0
-			j -= 1
+			local_n = 0
+			board_n += 1
 		else:
 			var x = "abcdefghi".find(s)
 			if x != -1:
@@ -253,33 +225,40 @@ func import_KEN() -> void:
 	set_next_board()
 	print_KEN()
 
-func move(x: int, y: int, button: Button = null) -> void:
-	if button == null:
-		button = $game_grid.get_child(y*9 + x)
+func move(board_number: int, local_n: int) -> void:
+	var button = $game_grid.get_child(board_number).get_child(local_n)
+	var local_x = local_n % 3
+	var local_y = local_n / 3
+	var global_position = board_number * 9 + local_n
+	var global_x = global_position%9
+	var global_y = global_position/9
+	var coordinate_x = board_number%3*3 + local_x
+	var coordinate_y = board_number/3*3 + local_y
 	
 	if who_turn == 'x':
-		field_x.set_bit(x, y, true)
+		field_x.set_bit(global_x, global_y, true)
 		button.add_theme_color_override("font_color", Color.RED)
 	else:
-		field_o.set_bit(x, y, true)
+		field_o.set_bit(global_x, global_y, true)
 		button.add_theme_color_override("font_color", Color.GREEN)
 	button.text = who_turn
-	last_move = "abcdefghi"[x] + str(9-y)
+	last_move = "abcdefghi"[coordinate_x] + str(9-coordinate_y)
 	change_turn()
 
 func print_KEN() -> String:
 	var count_empty_spaces = 0
 	var line = ""
 	
-	for j in range(9):
-		for i in range(9):
-			var j_reverse = 8 - j
-			var x = field_x.get_bit(i, j_reverse)
-			var o = field_o.get_bit(i, j_reverse)
-			if x:
+	for board_number in range(9):
+		var transformed_board_number = transform_to_reverse_y_order(board_number)
+		for local_n in range(9):
+			var transformed_local_n = transform_to_reverse_y_order(local_n)
+			var _x = field_x.get_bit(transformed_local_n, transformed_board_number)
+			var _o = field_o.get_bit(transformed_local_n, transformed_board_number)
+			if _x:
 				line += "x"
 				count_empty_spaces = 0
-			else: if o:
+			else: if _o:
 				line += "o"
 				count_empty_spaces = 0
 			else:
@@ -287,7 +266,7 @@ func print_KEN() -> String:
 				if !count_empty_spaces == 1:
 					line = line.rstrip("0123456789")
 				line += str(count_empty_spaces)
-		if j < 8:
+		if board_number < 8:
 			line += "-"
 		count_empty_spaces = 0
 	line += " " + str(last_move)
@@ -303,16 +282,28 @@ func print_win_count() -> void:
 	else: if win_count_o >= 3:
 		$label_result.text = "O wins"
 
-func remove_focus_on_board(i: int) -> void:
-	var board_color = boards_color[i/3][i%3]
-	if board_color == color0:
-		draw_bg_color(i, color0)
-	else: if board_color == color1:
-		draw_bg_color(i, color1)
-	else: if board_color == color2:
-		draw_bg_color(i, color2)
-	pass
+func remove_focus_on_board(board_number: int) -> void:
+	var board_color = boards_color[board_number]
+	draw_bg_color(board_number, board_color)
 
 func select_color_on_board(board_number: int, board_color: Color) -> void:
-	boards_color[board_number/3][board_number%3] = board_color
+	boards_color[board_number] = board_color
 	draw_bg_color(board_number, board_color)
+
+func set_next_board() -> void:
+	if (last_move != ""):
+		var next_board_x = "abcdefghi".find(last_move[0]) % 3
+		var next_board_y = 2 - (int(last_move[1]) - 1) % 3
+		var next_board_number = next_board_x + next_board_y*3
+		
+		if check_full_board(next_board_number) || boards_winner[next_board_number]:
+			$label_next_board/label_value.text = "any"
+			remove_focus_on_board(focused_board)
+		else:
+			$label_next_board/label_value.text = str(next_board_number+1)
+			focus_on_board(next_board_number)
+	
+	pass
+
+func transform_to_reverse_y_order(n: int) -> int:
+	return n % 3 + (2 - n / 3) * 3
